0c32b6eacd91511983251ebe8508be4e65009f85
João Murilo Daniel <joao_murilo@live.com>
2022-09-23 16:09:17 -0300

    Implementado o Spring Security no projeto

--- avaliacao-03/src/main/java/com/avaliacao03/avaliacao03/Avaliacao03Application.java
+++ avaliacao-03/src/main/java/com/avaliacao03/avaliacao03/Avaliacao03Application.java
@@ -2,8 +2,10 @@ package com.avaliacao03.avaliacao03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class Avaliacao03Application {
 
 	public static void main(String[] args) {
